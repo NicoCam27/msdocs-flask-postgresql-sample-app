dc3151842a9edc62d6b18989482cfdf8ba3a5974
NicoCam27 <98966957+NicoCam27@users.noreply.github.com>
2025-05-07 23:23:59 +0000

    stable with ability to see images from bd and add manually

--- models.py
+++ models.py
@@ -30,3 +30,17 @@ class Review(db.Model):
 
     def __str__(self):
         return f"{self.user_name}: {self.review_date:%x}"
+    
+
+class Imagen(db.Model):
+    __tablename__ = 'imagen'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(500)) #Nombre de Usuario
+    nombre_archivo = Column(String(500)) #Nombre del Archivo
+    n_pixeles_total = Column(Integer) #Nº Pixeles extraidos
+    tipo_transformacion = Column(String(50)) #Tipo de transformacion - Copia, Blanco y Negro, Pixelado, Identificacion de Colores
+    n_pixeles_azules = Column(Integer) #Nº Pixeles Azules
+    n_pixeles_verdes = Column(Integer) #Nº Pixeles Verdes
+    n_pixeles_rojos = Column(Integer) #Nº Pixeles Rojos
+    fecha = Column(DateTime) #Fecha y Hora
+
